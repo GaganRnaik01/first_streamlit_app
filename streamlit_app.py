09434048b70f5a54a8d660582eb79e081343b693
GaganRnaik01 <151710621+GaganRnaik01@users.noreply.github.com>
2023-11-22 22:10:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,18 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.text('fruityvise fruit advice')
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice=streamlit.text_input('what fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select the fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
